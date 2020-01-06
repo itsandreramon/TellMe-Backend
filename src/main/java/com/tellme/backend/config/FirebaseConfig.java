235612e63fe498d5c15c0a33158388f752837e81
andrethiele <thielea@th-brandenburg.de>
2020-01-06 22:16:14 +0100

    (Firebase) : Update serviceAccountKey.json path

--- src/main/java/com/tellme/backend/config/FirebaseConfig.java
+++ src/main/java/com/tellme/backend/config/FirebaseConfig.java
@@ -3,19 +3,16 @@ package com.tellme.backend.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("./serviceAccountKey.json")
-    private String serviceAccountKeyPath;
+    private String serviceAccountKeyPath = "./serviceAccountKey.json";
 
     @PostConstruct
     public void init() throws IOException {
