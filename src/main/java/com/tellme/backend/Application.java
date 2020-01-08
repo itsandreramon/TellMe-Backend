77dbf102d5ba4379fa7a1acf777cd97a4e421095
andrethiele <thielea@th-brandenburg.de>
2020-01-08 21:04:52 +0100

    (Project) : Undo JAR packaging

--- src/main/java/com/tellme/backend/Application.java
+++ src/main/java/com/tellme/backend/Application.java
@@ -10,6 +10,7 @@ package com.tellme.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
