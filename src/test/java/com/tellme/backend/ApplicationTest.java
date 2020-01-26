8a01dd05aa20931fa69bf7792fa48cfd9d25bfd7
andrethiele <thielea@th-brandenburg.de>
2020-01-26 14:08:17 +0100

    (Backend) : Add tests

---
+++ src/test/java/com/tellme/backend/ApplicationTest.java
@@ -0,0 +1,24 @@
+package com.tellme.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testHello() {
+        webTestClient
+                .get().uri("/")
+                .accept(MediaType.TEXT_PLAIN)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("Running...");
+    }
+}
