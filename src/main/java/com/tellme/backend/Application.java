a9d57d2b3d6153f2733662782a6b852d11d5e4f9
andrethiele <thielea@th-brandenburg.de>
2020-01-28 18:23:58 +0100

    (Backend) : Add CORS WebFilter and permit any

--- src/main/java/com/tellme/backend/Application.java
+++ src/main/java/com/tellme/backend/Application.java
@@ -9,11 +9,19 @@ package com.tellme.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CorsWebFilter corsFilter() {
+        return new CorsWebFilter(exchange -> new CorsConfiguration().applyPermitDefaultValues());
     }
 }
