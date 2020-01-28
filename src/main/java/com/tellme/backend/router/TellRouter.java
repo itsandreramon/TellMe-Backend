97bc9a49c1454d9895e3bb27598e58b29fef5abd
andrethiele <thielea@th-brandenburg.de>
2020-01-28 09:38:33 +0100

    (Backend) : Fix router formatting

--- src/main/java/com/tellme/backend/router/TellRouter.java
+++ src/main/java/com/tellme/backend/router/TellRouter.java
@@ -17,14 +17,20 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class TellRouter {
 
-	@Bean
-	RouterFunction<ServerResponse> tellRoutes(TellHandler handler) {
-		return RouterFunctions.route()
-				.path("/api/v2/",
-						b1 -> b1.path("/tells", b2 -> b2.GET("/", handler::findAll).GET("/id/{id}", handler::findById)
-								.GET("/sender/{senderUid}", handler::findBySenderUid)
-								.GET("/receiver/{receiverUid}", handler::findByReceiverUid).POST("/", handler::save)
-								.PUT("/", handler::update).DELETE("/id/{id}", handler::deleteById)))
-				.build();
-	}
+    @Bean
+    RouterFunction<ServerResponse> tellRoutes(TellHandler handler) {
+        return RouterFunctions.route()
+                .path("/api/v2/", b1 -> b1
+                        .path("/tells", b2 -> b2
+                                .GET("/", handler::findAll)
+                                .GET("/id/{id}", handler::findById)
+                                .GET("/sender/{senderUid}", handler::findBySenderUid)
+                                .GET("/receiver/{receiverUid}", handler::findByReceiverUid)
+                                .POST("/", handler::save)
+                                .PUT("/", handler::update)
+                                .DELETE("/id/{id}", handler::deleteById)
+						)
+				)
+                .build();
+    }
 }
--- src/main/java/com/tellme/backend/router/UserRouter.java
+++ src/main/java/com/tellme/backend/router/UserRouter.java
@@ -17,15 +17,23 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class UserRouter {
 
-	@Bean
-	RouterFunction<ServerResponse> routes(UserHandler handler) {
-		return RouterFunctions.route()
-				.path("/api/v2/", b1 -> b1.path("/users", b2 -> b2.GET("/", handler::findAll)
-						.GET("/uid/{uid}", handler::findByUid).GET("/username/{username}", handler::findByUsername)
-						.GET("/search/{query}/{limit}", handler::findByUsernameLike)
-						.GET("/uid/{uid}/feed", handler::getFeedByUid).GET("/uid/{uid}/inbox", handler::getInboxByUid)
-						.GET("/uid/{uid}/auth", handler::findAuthUserByUid).POST("/", handler::save)
-						.PUT("/", handler::update).DELETE("/uid/{uid}", handler::deleteByUid)))
-				.build();
-	}
+    @Bean
+    RouterFunction<ServerResponse> routes(UserHandler handler) {
+        return RouterFunctions.route()
+                .path("/api/v2/", b1 -> b1
+                        .path("/users", b2 -> b2
+                                .GET("/", handler::findAll)
+                                .GET("/uid/{uid}", handler::findByUid)
+                                .GET("/username/{username}", handler::findByUsername)
+                                .GET("/search/{query}/{limit}", handler::findByUsernameLike)
+                                .GET("/uid/{uid}/feed", handler::getFeedByUid)
+                                .GET("/uid/{uid}/inbox", handler::getInboxByUid)
+                                .GET("/uid/{uid}/auth", handler::findAuthUserByUid)
+                                .POST("/", handler::save)
+                                .PUT("/", handler::update)
+                                .DELETE("/uid/{uid}", handler::deleteByUid)
+                        )
+                )
+                .build();
+    }
 }
