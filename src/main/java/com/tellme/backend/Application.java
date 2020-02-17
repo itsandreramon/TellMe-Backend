3a0a3a317f9c2e73071f9dea5f67b488d1563d5b
André Thiele <thielea@th-brandenburg.de>
2020-02-17 23:54:16 +0100

    (Backend) : Add ReactorDebugAgent

--- src/main/java/com/tellme/backend/Application.java
+++ src/main/java/com/tellme/backend/Application.java
@@ -9,11 +9,13 @@ package com.tellme.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        ReactorDebugAgent.init();
         SpringApplication.run(Application.class, args);
     }
 }
